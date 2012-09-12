4c11218a97d1a8e2ddd216c128c2276ce524feb4
Brian Nickel <brian.nickel@gmail.com>
2012-09-11 23:31:52 -0700

    Fix: Use correct source for new-trim.js

--- test/acceptance-criteria/samples/new-trim.js
+++ test/acceptance-criteria/samples/new-trim.js
@@ -1,33 +1,3 @@
-var gitPerfTest = (function () {
-    
-    
-    
-    
-    
-}());
-
-
-var VersionedScriptLoader = (function() {
-    function VersionedScriptLoader(format) {
-        
-    }
-    
-    VersionedScriptLoader.prototype = {
-        onComplete: null,
-        scriptLoaded:
-    };
-    
-} ());
-
-loadScripts({
-    pattern: 'https://raw.github.com/madrobby/zepto/{version}/src/zepto.js',
-    versions: [
-            'd86df6e4b0ea123d326238cc881a6c73eeab0076'
-        ],
-    afterEach: function (version, remaining) {
-            
-        },
-    complete: function () {
-            console.log('finished');
-        }
-});
+function trim(src) {
+    return src.trim();
+}
